8053ea15c55fe03a9ac6697d2c431d8d2decbad0
scott <scott@sboss.net>
2012-03-21 00:42:00 -0400

    init commit.

---
+++ README.md
@@ -0,0 +1,13 @@
+# README
+
+**warning**: this is very bad ruby code.  I am a n00b to ruby.
+
+this script will download the coderwall badges that you have.
+
+	syntax:
+	ruby coderwall.rb <username>
+	
+	i.e. 
+	ruby coderwall.rb sboss
+
+the script will name the image files as \<badge name\>.png.
---
+++ coderwall.rb
@@ -0,0 +1,30 @@
+# v0.1
+# written (very badly) by scott boss
+#
+#
+require 'json'
+require 'net/http'
+#
+username = ARGV[0]
+#
+if username.nil? then
+  puts "no name provided"
+  puts $0 + " coderwall_username"
+else
+base_url = "http://coderwall.com/" + username + ".json"
+resp     = Net::HTTP.get_response( URI.parse( base_url ) )
+data     = resp.body
+result   = JSON.parse( data )
+#puts result.class
+result["badges"].each { |i|
+  uri = URI.parse( i["badge"] )
+  Net::HTTP.start( uri.host,uri.port ) do |http|
+    filename = i["name"] + ".png"
+    response = http.get( uri.path )
+    open( filename, "wb" ) do |file|
+      file.write( response.body )
+    end
+  end
+  puts i["name"] + " -> " + i["description"]
+  }
+end
